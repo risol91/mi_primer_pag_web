59dd6aa2f723ff8ef0814dcbee2dd8b3d6f22d58
risol91 <juanrisol84@hotmail.com>
2023-11-02 20:18:14 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "Sebastian";
+//nombre = "maria";
+
+//var nombre1 = "marta";
+
+//const nombre2 = "marta";
+//nombre2 = "maria"
+
+//console.log (nombre);
+//console.log (nombre1);
+//console.log (nombre2);
+
+// SELECCIONAR ELEMENTOS
+
+let contenidoTitulo = "nombre";
+
+let titulo = document.querySelector(".nombre")
